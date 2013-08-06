7faa67a051ca015879beab8cb33274599e927032
miketahani <tahani@stamen.com>
2013-08-06 11:11:11 -0700

    other

--- play/gather/gather.js
+++ play/gather/gather.js
@@ -41,10 +41,20 @@ function getFlickrData(bbox, page, photos, callback) {
   }
 
   console.log("getting page", page);
-  var url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f17cc9d3f73f0b45640451a6d3c1946d&has_geo=1&extras=geo%2Ctags&format=json&nojsoncallback=1";
-  url += "&bbox=" + bbox;
-  url += "&min_taken_date=" + (~~(Date.now() / 1000) - 60*60*24*365);
-  url += "&page=" + page;
+  var url = {
+    url: "http://api.flickr.com/services/rest",
+    qs: {
+      method: "flickr.photos.search",
+      api_key: "f17cc9d3f73f0b45640451a6d3c1946d",
+      bbox: bbox,
+      has_geo: 1,
+      extras: "geo,tags",
+      min_taken_date: ~~(Date.now() / 1000) - 60*60*24*365,
+      format: "json",
+      nojsoncallback: 1,
+      page: page,
+      per_page: 250   // max = 250 for geo queries
+  };
   request(url, function (error, response, body) {
     
     if (!error && response.statusCode == 200) {
@@ -107,13 +117,13 @@ var getBoundingBoxForPark = function(park, callback) {
   // connect to pg
   // query pg
   // callback with parsed bbox
-  var query = "select unit_name, st_astext(st_envelope(st_transform(st_buffer(st_envelope(geom), 500), 4326))) as envelope from cpad_units where ogc_fid = $1 limit 1";
+  var query = ["select unit_name, st_astext(st_envelope(st_transform(st_buffer(st_envelope(geom), 500), 4326)))",
+               "as envelope from cpad_units where ogc_fid = $1 limit 1"].join("");
 
   return client.query(query, [park.id], function(err, res) {
     if (err) {
       throw err;
     }
-
     var envelope = wkt2bbox(res.rows[0]);
     client.end();
     return callback(null, envelope);
@@ -126,7 +136,7 @@ var getBoundingBoxForPark = function(park, callback) {
 getFlickrPhotosForPark({
   id: 9850
 }, function(err, media) {
-  console.log("These photos were taken in South Yuba River State Park: %j", media);
+  console.log("These photos were taken in South Yuba River State Park: %j", media.length);
 });
 
 // [1,2,3].forEach(function(x) {
