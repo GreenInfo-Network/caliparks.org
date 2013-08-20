4e2a3cc28f53f130c1b7e0cad66cf479ff3f1533
miketahani <tahani@stamen.com>
2013-08-20 14:39:59 -0700

    yay concating html strings

--- play/gather/fuckit.py
+++ play/gather/fuckit.py
@@ -3,6 +3,7 @@
 from os import listdir
 import json
 
+
 basedir = "data/"
 url = "http://farm%s.staticflickr.com/%s/%s_%s_s.jpg"
 files = [f for f in listdir(basedir) if ".json" in f]
@@ -10,9 +11,11 @@ for filename in files:
     html = "<html><head><style>img { width: 75px; height: 75px;  }</style></head><body>"
     with open(basedir + filename) as infile:
         photos = json.load(infile)
-        imgs = map(lambda p: url % (p["farm"], p["server"], p["id"], p["secret"]), photos)
+	if not photos: continue
+	html += "<h1>%s</h1>" % (photos[0]["park"]["name"])
+ 	imgs = map(lambda p: url % (p["farm"], p["server"], p["id"], p["secret"]), photos)
         html += "\n".join(["<img src='%s'>" % img for img in imgs]) + "</body></html>"
         with open("gallery/" + filename.replace(".json", ".html"), "w") as outfile:
-            outfile.write(html)
+            outfile.write(html.encode('ascii','ignore'))
         print "done", filename
         
