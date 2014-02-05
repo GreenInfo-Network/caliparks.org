8c48a97a4567ac9876f270817ba657de62c731f2
Eric Gelinas <evg@stamen.com>
2014-02-05 11:57:00 -0800

    add large park shapes to homepage

--- public/js/view_park.js
+++ public/js/view_park.js
@@ -2,7 +2,7 @@
 
 (function(window) {
 
-  window.STMN = {};
+  window.STMN = window.STMN || {};
 
   var bounds = bounds;
 
@@ -59,84 +59,6 @@
 
   }
 
-	function displayPark(rootSelector, shapeData, options) {
-
-		options = options || {
-			//defaults
-			canvasWidth  : 110,
-			canvasWeight : 110,
-			scale         : 600,
-			fillColor    : "rgb(34, 17, 2)",
-			dotLocation  : null,
-			dotRadius    : 2,
-			dotColor     : 'white'
-		};
-
-
-		//
-		// Fit postgres output into Feature collection
-		// boilerplate
-		//
-		var geoJSON = {
-		    "type": "FeatureCollection",
-		    "features":[
-		        {
-		            "type":"Feature",
-		            "geometry":{
-		                "type":"MultiPolygon",
-		                "coordinates":shapeData.coordinates
-		            },
-		            "properties":{
-		                "name" : "bar"
-		            }
-		        }
-		    ]
-		};
-
-
-		var width = 300, height = 300, initialScale = 1000;
-
-		//
-		// Project the feature at a default scale
-		//
-      // create a first guess for the projection
-      var center = d3.geo.centroid(geoJSON)
-      var scale  = 150;
-      var offset = [width/2, height/2];
-      var projection = d3.geo.mercator().scale(scale).center(center)
-          .translate(offset);
-
-      // create the path
-      var path = d3.geo.path().projection(projection);
-
-      // using the path determine the bounds of the current map and use 
-      // these to determine better values for the scale and translation
-      var bounds  = path.bounds(geoJSON);
-      var hscale  = scale*width  / (bounds[1][0] - bounds[0][0]);
-      var vscale  = scale*height / (bounds[1][1] - bounds[0][1]);
-      var scale   = (hscale < vscale) ? hscale : vscale;
-      var offset  = [width - (bounds[0][0] + bounds[1][0])/2,
-                        height - (bounds[0][1] + bounds[1][1])/2];
-
-      // new projection
-      projection = d3.geo.mercator().center(center)
-        .scale(scale-1500).translate(offset);
-      path = path.projection(projection);
-
-      var vis = d3.select("#park-shape").append("svg")
-      .attr("width", width).attr("height", height)
-
-      // add a rectangle to see the bound of the svg
-      vis.append("rect").attr('width', width).attr('height', height)
-        .style('fill', 'none');
-
-      vis.selectAll("path").data(geoJSON.features).enter().append("path")
-        .attr("d", path)
-        .style("fill", "rgb(145, 137, 106)")
-
-
-	}
-
   function initView(data) {
 
     if (data.UsCaShape.display) {
@@ -146,12 +68,13 @@
       );
     }
 
-    displayPark(
+    var parkShape = new STMN.ParkShape(
       data.parkShape.rootSelector, 
       data.parkShape.shapeData, 
       data.parkShape.options
     );
 
+    parkShape.init();
   }
 
   //Public interface
