b1f1795d3bd5853b109deb463833a3aa6af1c10d
Eric Gelinas <evg@stamen.com>
2014-02-21 16:39:41 -0800

    fix agency display

--- controllers/park.js
+++ controllers/park.js
@@ -35,55 +35,63 @@ module.exports = function(req, res, data, callback) {
         if(err) {
           return console.error('error running query', err);
         }
+
+          pgClient.query('select * from site_instagram_photos where su_id = ' + park_id, function(err, instasult) {
+            if(err) {
+              return console.error('error running query', err);
+            }
+
+            //
+            // Was a park found? if not, just 404
+            //
+            if (result.rows[0]) {
+
+              //
+              // Get tweets
+              //
+              tweets_all      = require('../public/data/park_tweets.json'),
+              tweets_filtered = [];
+
+              tweets_all.features.forEach(function(tweet) {
+
+                if (tweet.properties.park_name === result.rows[0].unit_name && tweet_iteration < 20) {
+                  tweet_iteration++;
+                  tweets_filtered.push(tweet.properties);
+                }
+
+              });
+
+              /*
+              var bbox     = JSON.parse(result.rows[0].bbox),
+                  distance = (gpsUtil.getDistance(bbox.coordinates[0][3][1], bbox.coordinates[0][3][0], bbox.coordinates[0][4][1], bbox.coordinates[0][4][0])/69);
+              console.log(distance + ' Miles');
+              */
+
+              callback( null, {
+                appTitle         : 'Stamen Parks: California > ' + result.rows[0].unit_name,
+                park_id          : result.rows[0].su_id,
+                name             : result.rows[0].unit_name,
+                agency_slug      : result.rows[0].agncy_name.split(' ').join('+').split(',')[0],
+                totalPhotos      : flesult.rows.length ? flesult.rows.length : 0,
+                coverPhoto       : flesult.rows.length ? flesult.rows[0] : null,
+                locationDisplay  : {
+                  lat : gpsUtil.getDMSLatitude(result.rows[0].centroid_latitude),
+                  lon : gpsUtil.getDMSLongitude(result.rows[0].centroid_longitude)
+                },
+                cpadPark         : result.rows[0],
+                hashtag          : hashtags[result.rows[0].su_id],
+                tweets           : tweets_filtered,
+                tweet_count      : tweets_filtered.length,
+                top_instagram_photos : instasult.rows
+              } );
+
+            } else {
+              callback( null, null );
+            }
+          
+            pgClient.end();
+          });
       
-  				//
-  				// Was a park found? if not, just 404
-  				//
-  				if (result.rows[0]) {
-
-            //
-            // Get tweets
-            //
-            tweets_all      = require('../public/data/park_tweets.json'),
-            tweets_filtered = [];
-
-            tweets_all.features.forEach(function(tweet) {
-
-              if (tweet.properties.park_name === result.rows[0].unit_name && tweet_iteration < 20) {
-                tweet_iteration++;
-                tweets_filtered.push(tweet.properties);
-              }
-
-            });
-
-            /*
-            var bbox     = JSON.parse(result.rows[0].bbox),
-                distance = (gpsUtil.getDistance(bbox.coordinates[0][3][1], bbox.coordinates[0][3][0], bbox.coordinates[0][4][1], bbox.coordinates[0][4][0])/69);
-            console.log(distance + ' Miles');
-            */
-
-  					callback( null, {
-  						appTitle        : 'Stamen Parks: California > ' + result.rows[0].unit_name,
-              park_id         : result.rows[0].su_id,
-              name            : result.rows[0].unit_name,
-              agency_slug     : result.rows[0].agncy_name.split(' ').join('+').split(',')[0],
-  				 		totalPhotos     : flesult.rows.length ? flesult.rows.length : 0,
-  				 		coverPhoto      : flesult.rows.length ? flesult.rows[0] : null,
-  				 		locationDisplay : {
-  				 			lat : gpsUtil.getDMSLatitude(result.rows[0].centroid_latitude),
-  				 			lon : gpsUtil.getDMSLongitude(result.rows[0].centroid_longitude)
-  				 		},
-  				 		cpadPark        : result.rows[0],
-              hashtag         : hashtags[result.rows[0].su_id],
-              tweets          : tweets_filtered,
-              tweet_count     : tweets_filtered.length
-  					} );
-
-  				} else {
-  					callback( null, null );
-  				}
-  			
-          pgClient.end();
         });
 			});
 		});
--- controllers/parks.js
+++ controllers/parks.js
@@ -25,7 +25,7 @@ module.exports = function(req, res, data, callback) {
 		});
 
 		return callback(null, {
-			appTitle : 'Stamen Parks: ' + contextData.title,
+			appTitle : 'California parks: ' + contextData.title,
 		 	parks    : contextDataDecorated,
 		 	empty    : !(contextDataDecorated.length)
 		});
