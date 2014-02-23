ad77bf81daae915671e65794dc720310d9a5b94c
Eric Gelinas <evg@stamen.com>
2014-02-23 12:45:27 -0800

    get more twitter data

--- controllers/park.js
+++ controllers/park.js
@@ -50,62 +50,54 @@ module.exports = function(req, res, data, callback) {
                 return console.error('error running query', err);
               }
 
-              //
-              // Was a park found? if not, just 404
-              //
-              if (result.rows[0]) {
+              pgClient.query('select * from site_tweets where su_id = ' + park_id, function(err, tweetsult) {
+                if(err) {
+                  return console.error('error running query', err);
+                }
 
                 //
-                // Get tweets
+                // Was a park found? if not, just 404
                 //
-                tweets_all      = require('../public/data/park_tweets.json'),
-                tweets_filtered = [];
+                if (result.rows[0]) {
 
-                tweets_all.features.forEach(function(tweet) {
-
-                  if (tweet.properties.park_name === result.rows[0].unit_name && tweet_iteration < 20) {
-                    tweet_iteration++;
-                    tweets_filtered.push(tweet.properties);
-                  }
-
-                });
-
-                //
-                // Get checkins and tips count from Foursquare
-                //
-                foursult.rows.forEach(function(venue) {
-                  foursquare_checkins += venue.checkinscount;
-                  foursquare_tips += venue.tipcount;
-                });
+                  //
+                  // Get checkins and tips count from Foursquare
+                  //
+                  foursult.rows.forEach(function(venue) {
+                    foursquare_checkins += venue.checkinscount;
+                    foursquare_tips += venue.tipcount;
+                  });
 
 
-                callback( null, {
-                  appTitle         : 'Stamen Parks: California > ' + result.rows[0].unit_name,
-                  park_id          : result.rows[0].su_id,
-                  name             : result.rows[0].unit_name,
-                  agency_slug      : result.rows[0].agncy_name.split(' ').join('+').split(',')[0],
-                  totalPhotos      : flesult.rows.length ? flesult.rows.length : 0,
-                  coverPhoto       : flesult.rows.length ? flesult.rows[0] : null,
-                  locationDisplay  : {
-                    lat : gpsUtil.getDMSLatitude(result.rows[0].centroid_latitude),
-                    lon : gpsUtil.getDMSLongitude(result.rows[0].centroid_longitude)
-                  },
-                  cpadPark         : result.rows[0],
-                  hashtag          : hashtags[result.rows[0].su_id],
-                  tweets           : tweets_filtered,
-                  tweet_count      : tweets_filtered.length,
-                  top_instagram_photos : instasult.rows,
-                  venues_activity  : foursult.rows,
-                  venues_count     : foursult.rows.length < 100000 ? numeral(foursult.rows.length).format('0,0') : 'Loads!',
-                  venues_checkins  : foursquare_checkins < 100000 ? numeral(foursquare_checkins).format('0,0') : 'Loads!',
-                  venues_tips      : foursquare_tips < 100000 ? numeral(foursquare_tips).format('0,0') : 'Loads!'
-                } );
+                  callback( null, {
+                    appTitle         : 'Stamen Parks: California > ' + result.rows[0].unit_name,
+                    park_id          : result.rows[0].su_id,
+                    name             : result.rows[0].unit_name,
+                    agency_slug      : result.rows[0].agncy_name.split(' ').join('+').split(',')[0],
+                    totalPhotos      : flesult.rows.length ? flesult.rows.length : 0,
+                    coverPhoto       : flesult.rows.length ? flesult.rows[0] : null,
+                    locationDisplay  : {
+                      lat : gpsUtil.getDMSLatitude(result.rows[0].centroid_latitude),
+                      lon : gpsUtil.getDMSLongitude(result.rows[0].centroid_longitude)
+                    },
+                    cpadPark         : result.rows[0],
+                    hashtag          : hashtags[result.rows[0].su_id],
+                    tweets           : tweetsult.rows,
+                    tweet_count      : tweetsult.rows.length,
+                    top_instagram_photos : instasult.rows,
+                    venues_activity  : foursult.rows,
+                    venues_count     : foursult.rows.length < 100000 ? numeral(foursult.rows.length).format('0,0') : 'Loads!',
+                    venues_checkins  : foursquare_checkins < 100000 ? numeral(foursquare_checkins).format('0,0') : 'Loads!',
+                    venues_tips      : foursquare_tips < 100000 ? numeral(foursquare_tips).format('0,0') : 'Loads!'
+                  } );
 
-              } else {
-                callback( null, null );
-              }
-            
-              pgClient.end();
+                } else {
+                  callback( null, null );
+                }
+              
+                pgClient.end();
+
+              });
 
             });
 
