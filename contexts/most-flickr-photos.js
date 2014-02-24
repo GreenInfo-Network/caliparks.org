1d087c729eb40a1c693fc8baa559c798320932d4
Eric Gelinas <evg@stamen.com>
2014-02-24 14:58:03 -0800

    add a most flickr context

--- contexts/most-flickr-photos.js
+++ contexts/most-flickr-photos.js
@@ -2,13 +2,47 @@
 
 module.exports = function(data, callback) {
 
-  var context = require('../public/data/context-flickr-photos.json');
+  var pg = require('pg');
 
-  console.log('context',context);
+  var context   = require('../public/data/context-flickr-photos.json'),
+      match_map = {};
 
-  callback(null, {
-    parks : context,
-    title : 'Most Flickr photos'
-  });
+  function finish(context) {
+
+    callback(null, {
+      parks : context,
+      title : 'Most Flickr photos'
+    });
+
+  }
+
+  //
+  // If the caller already has an array of parks it can be
+  // passed in an ordered. Otherwise, the db will be called
+  // for the park metadata
+  //
+  if (typeof data.mixData === 'array') {
+
+      finish(context);
+
+  } else {
+    pg.connect('postgres://eric@localhost/eric', function(err, client, cb) {
+
+      client.query('select su_id, unit_name from site_park', function(err, response) {
+
+        response.rows.forEach(function(park) {
+          match_map[park.su_id] = park;
+        });
+
+        finish(context.map(function(item) {
+          //
+          // Actually just return the db record
+          //
+          return match_map[item.su_id];
+        }));
+      });
+
+    })
+  }
 
 }
