2e19d27021d6eb00e99f85a764340de802c6b510
Eric Gelinas <evg@stamen.com>
2014-02-24 16:39:12 -0800

    move instagram and flickr to use factory

--- contexts/most-flickr-photos.js
+++ contexts/most-flickr-photos.js
@@ -1,48 +1,9 @@
 'use strict';
 
-module.exports = function(data, callback) {
+var flatFileContext = new require('../library/flat-file-context.js')({
+  name  : 'flickr-photos',
+  query : 'select su_id, unit_name from site_park',
+  title : 'Most Flickr photos'
+});
 
-  var pg = require('pg');
-
-  var context   = require('../public/data/context-flickr-photos.json'),
-      match_map = {};
-
-  function finish(context) {
-
-    callback(null, {
-      parks : context,
-      title : 'Most Flickr photos'
-    });
-
-  }
-
-  //
-  // If the caller already has an array of parks it can be
-  // passed in an ordered. Otherwise, the db will be called
-  // for the park metadata
-  //
-  if (typeof data.mixData === 'array') {
-
-      finish(context);
-
-  } else {
-    pg.connect('postgres://eric@localhost/eric', function(err, client, cb) {
-
-      client.query('select su_id, unit_name from site_park', function(err, response) {
-
-        response.rows.forEach(function(park) {
-          match_map[park.su_id] = park;
-        });
-
-        finish(context.map(function(item) {
-          //
-          // Actually just return the db record
-          //
-          return match_map[item.su_id];
-        }));
-      });
-
-    })
-  }
-
-}
+module.exports = flatFileContext;
