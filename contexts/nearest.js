8a39e31bc13e70c1ce88946c9066222884fa6cbe
Eric Gelinas <evg@stamen.com>
2014-03-24 13:45:47 -0700

    forward geocoding

--- contexts/nearest.js
+++ contexts/nearest.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = function(data, callback) {
-  var loc;
 
   var pg      = require('pg'),
       request = require('request');
@@ -11,39 +10,67 @@ module.exports = function(data, callback) {
 
   var dbLimit = '',
       dbQuery = '',
-      thisPlace;
+      isLatLongTest = /^-?\d*.\d*\,-?\d*.\d*$/,
+      thisPlace, loc, res;
 
-    pgClient.connect(function(err) {
+  function getPlace(callback) {
+
+    if (data.query && isLatLongTest.test(data.query)) {
+      loc = data.query.split(',');
+
+      request('http://api.tiles.mapbox.com/v3/stamen.hckn2ljm/geocode/'+loc[1]+','+loc[0]+'.json', function (error, response, body) {
+        if (error) {
+          return callback(error);
+        }
+
+        callback(null, {
+          'coordinates' : loc,
+          'details'     : JSON.parse(body).results[0][0]
+        });
+      });
+    } else if(data.query) {
+
+      request('http://api.tiles.mapbox.com/v3/stamen.hckn2ljm/geocode/'+data.query+'.json', function (error, response, body) {
+        if (error) {
+          return callback(error);
+        }
+
+        res = JSON.parse(body).results[0][0];
+
+        callback(null, {
+          'coordinates' : [res.lat, res.lon],
+          'details'     : res
+        });
+      });
+
+    } else {
+      callback(null,null);
+    }
+
+  }
+
+  pgClient.connect(function(err) {
 
     if(err) {
       return console.error('could not connect to postgres', err);
     }
 
     if (data.query) {
-      loc = data.query.split(',');
-    }
 
-    if (data.query) {
+      getPlace(function(error, place) {
+        pgClient.query('select *, ST_distance(geom, st_setsrid(st_makepoint('+place.coordinates[1]+','+place.coordinates[0]+'),4326)) as distance from (select * from cpad_2013b_superunits_ids_4326 where ST_DWithin(geom, st_setsrid(st_makepoint('+place.coordinates[1]+','+place.coordinates[0]+'),4326), .2)) as shortlist order by distance asc;', function(err, result) {
+          if(err) {
+            return console.error('error running query', err);
+          }
 
-      request('http://api.tiles.mapbox.com/v3/stamen.hckn2ljm/geocode/'+loc[1]+','+loc[0]+'.json', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-
-            thisPlace = JSON.parse(body).results[0][0];
-
-            pgClient.query('select *, ST_distance(geom, st_setsrid(st_makepoint('+loc[1]+','+loc[0]+'),4326)) as distance from (select * from cpad_2013b_superunits_ids_4326 where ST_DWithin(geom, st_setsrid(st_makepoint('+loc[1]+','+loc[0]+'),4326), .2)) as shortlist order by distance asc;', function(err, result) {
-              if(err) {
-                return console.error('error running query', err);
-              }
-
-              callback(null, {
-                parks : result.rows,
-                title : 'near ' + thisPlace.name
-              });
+          callback(null, {
+            parks : result.rows,
+            title : 'near ' + place.details.name
+          });
 
 
-              pgClient.end();
-            });
-        }
+          pgClient.end();
+        });
       });
 
     } else {
