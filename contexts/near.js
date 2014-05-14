97cfbef1d86aa56608401dfe1677ebb19de82d9a
Seth Fitzsimmons <seth@mojodna.net>
2014-05-14 12:57:07 -0700

    TODO
    This counts as a credential.

--- contexts/near.js
+++ contexts/near.js
@@ -16,6 +16,7 @@ module.exports = function(data, callback) {
     if (data.query && isLatLongTest.test(data.query)) {
       loc = data.query.split(',');
 
+      // TODO fetch the map id from the environment
       request('http://api.tiles.mapbox.com/v3/stamen.hckn2ljm/geocode/'+loc[1]+','+loc[0]+'.json', function (error, response, body) {
         if (error) {
           return callback(error);
