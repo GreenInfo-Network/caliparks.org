aa2c1ccdc5801baa6802008c8bd46c1bd22b1f2f
Eric Gelinas <evg@stamen.com>
2014-06-30 11:36:33 -0700

    move to pg.connect pattern

--- controllers/agencies.js
+++ controllers/agencies.js
@@ -1,30 +1,28 @@
 'use strict';
 
+var env = require('require-env'),
+    pg  = require('pg');
+
 module.exports = function(req, res, data, callback) {
 
-  var pg      = require('pg');
-
-  var dbCon    = process.env.DATABASE_URL,
-        pgClient = new pg.Client(dbCon);
-
-    pgClient.connect(function(err) {
+  pg.connect(env.require('DATABASE_URL'), function(err, client, done) {
 
     if(err) {
       return console.error('could not connect to postgres', err);
     }
 
-    pgClient.query('SELECT agncy_id, agncy_name, count(agncy_id) as park_count FROM site_park GROUP BY agncy_id, agncy_name ORDER BY agncy_name ASC;', function(err, result) {
+    client.query('SELECT agncy_id, agncy_name, count(agncy_id) as park_count FROM site_park GROUP BY agncy_id, agncy_name ORDER BY agncy_name ASC', function(err, result) {
       if(err) {
         return console.error('error running query', err);
       }
+
+      done();
 
       callback(null, {
         app_title : 'California Open Spaces',
         agencies  : result.rows
       });
 
-
-      pgClient.end();
     });
 
   });
--- controllers/agency.js
+++ controllers/agency.js
@@ -1,21 +1,21 @@
 'use strict';
 
+var env = require('require-env'),
+    pg  = require('pg');
+
 module.exports = function(req, res, data, callback) {
 
 	var pg      = require('pg');
 
-	var dbCon  = process.env.DATABASE_URL,
-        pgClient = new pg.Client(dbCon);
+  var agency_name_parts = null;
 
-    var agency_name_parts = null;
-
-	pgClient.connect(function(err) {
+	pg.connect(env.require('DATABASE_URL'), function(err, client, done) {
 
 		if(err) {
 			return console.error('could not connect to postgres', err);
 		}
 
-		pgClient.query({
+		client.query({
 			text   : 'SELECT * FROM site_park where agncy_id = $1 ORDER BY agncy_name ASC;',
 			values : [req.params.id]
 		}, function(err, result) {
@@ -25,14 +25,13 @@ module.exports = function(req, res, data, callback) {
 
 			agency_name_parts = result.rows[0].agncy_name.split(',');
 
+			done();
+
 			callback(null, {
 				appTitle : 'California Open Spaces > Parks within ' + (agency_name_parts.length > 1 ? agency_name_parts[1] + ' ' + agency_name_parts[0] : agency_name_parts[0]),
 			 	name     : result.rows[0].agncy_name,
 			 	parks    : result.rows
 			});
-
-
-			pgClient.end();
 		});
 
 	});
