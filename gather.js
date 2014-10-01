3d92ee247a843e4ffd13d44a1b6ed54791a365ce
Seth Fitzsimmons <seth@mojodna.net>
2014-10-01 12:15:13 -0700

    Extract Flickr functionality

--- gather.js
+++ gather.js
@@ -11,6 +11,8 @@ var async = require("async"),
     env = require("require-env"),
     jsts = require("jsts");
 
+var flickr = require("./lib/flickr");
+
 var factory = new jsts.geom.GeometryFactory();
 var reader = new jsts.io.WKTReader(factory);
 
@@ -473,205 +475,6 @@ function instagramRecursionQueueTask(err, client, y, x, radius, polygon, parks, 
     });
   });
 }
-
-function hasMorePages(body) {
-  return body.photos.page < body.photos.pages;
-}
-
-function getFlickrData(bbox, page, photos, callback) {
-  if (arguments.length < 4) {
-    callback = arguments[arguments.length-1];
-    page = 1;
-    photos = [];
-  }
-
-  console.log("[*] getting page", page);
-
-  // TODO safe_search: 1
-  // TODO content_type: 1
-  // TODO media: photos
-  var url = {
-    url: "https://api.flickr.com/services/rest",
-    qs: {
-      method: "flickr.photos.search",
-      api_key: env.require("FLICKR_CLIENT_ID"),
-      bbox: bbox,
-      has_geo: 1,
-      extras: "geo,tags,date_upload,date_taken,owner_name,description,license,url_s,url_m,url_n,url_z,url_c,url_l,url_o",
-      min_taken_date: ~~(Date.now() / 1000) - 60*60*24*365*30,
-      //min_taken_date: ~~(Date.now() / 1000) - 60*60*24*365,
-      format: "json",
-      nojsoncallback: 1,
-      page: page,
-      per_page: 250   // max = 250 for geo queries
-    }
-  };
-  request(url, function (error, response, body) {
-
-    if (error) {
-      return callback(error);
-    }
-
-    if (!error && response.statusCode == 200) {
-      body = JSON.parse(body);
-
-      photos = photos.concat(body.photos.photo);
-
-      if (hasMorePages(body) && page <= body.photos.page && page < 100) {      //Stop after 100. TODO: do this better.
-        console.log("[*] found another page for", bbox , ":", body.photos.page, "out of", body.photos.pages);
-        console.log("[*] current page:", page);
-        return getFlickrData(bbox, page + 1, photos, callback);
-
-      } else {
-        return callback(null, photos, page);  // callback(err, result)
-      }
-    }
-
-    if (!error && response.statusCode == 403) {
-      console.log("caught code 403 forbidden");
-      body = JSON.parse(body);
-      console.log("body", body);
-      return callback(body); // Return body as error
-    }
-
-    if (!error && response.statusCode != 200) {
-      try {
-        body = JSON.parse(body);
-        console.log("caught not 200:", body);
-        return callback(body); // Return body as error
-      } catch (e) {
-        // if JSON parsing fails
-        console.log("caught not 200");
-        return callback(e);
-      }
-    }
-
-    return callback();
-  });
-
-}
-
-/**
- * Fetch media associated with a specified park.
- *
- * @param park Object{id} Park identifier.
- * @param callback Function(err, media[]) Called with a list of media associated with a park.
- */
- // add error checking at every level
-var getFlickrPhotosForPark = function(client, park, callback) {
-  // return startPostgresClient(function(err, client) {
-    return getBoundingBoxForPark(client, park, function(err, bbox) {
-      // test bounding box against existing metadata?
-      return getFlickrData(bbox, function(err, photos, pages) {
-        var bboxArray = bbox.split(",");
-        var lngMin = bboxArray[0],
-            latMin = bboxArray[1],
-            lngMax = bboxArray[2],
-            latMax = bboxArray[3];
-        var metadata_id = saveFlickrHarvesterMetadata(client, park.id, latMin, lngMin, latMax, lngMax, new Date(), photos.length, pages);
-        saveFlickrHarvesterResults(client, metadata_id, photos, park);
-        return callback(null, photos, pages);  // everything finished
-      });
-    });
-  // });
-};
-
-var getFlickrPhotosForBbox = function(client, latMin, lngMin, latMax, lngMax, callback) {
-  // Need to reorder the lat/lng to match expected format of bbox
-  return getFlickrData([lngMin,latMin,lngMax,latMax].join(","), function(err, photos, pages) {
-/*
-    var bboxArray = bbox.split(",");
-    var lngMin = bboxArray[0],
-        latMin = bboxArray[1],
-        lngMax = bboxArray[2],
-        latMax = bboxArray[3];
-*/
-    var length = photos ? photos.length : 0;
-    var metadata_id = saveFlickrHarvesterMetadata(client, null, latMin, lngMin, latMax, lngMax, new Date(), length, pages);
-    saveFlickrHarvesterResults(client, metadata_id, photos, null);
-    return callback(null, photos, pages);  // everything finished
-  });
-};
-
-var saveFlickrHarvesterMetadata = function(client, park_id, latMin, lngMin, latMax, lngMax, timestamp, count, pages) {
-  var query = "insert into flickr_metadata (su_id, latmin, lngmin, latmax, lngmax, date, count, pages, the_geom) values ($1, $2, $3, $4, $5, $6, $7, $8, ST_MakeEnvelope($3,$2,$5,$4,4326))";
-  return client.query(query, [park_id, latMin, lngMin, latMax, lngMax, timestamp, count, pages], function(err, res) {
-    if (err) {
-      console.log("harvesterMetadata error", err);
-      throw err;
-    }
-    return 0; // TODO: change these
-
-  });
-
-  return 1; // TODO: change these
-};
-
-
-var saveFlickrHarvesterResults = function(client, metadata_id, photos, park) {
-  var query = "INSERT INTO flickr_photos (photoid, owner, secret, server, farm, title, latitude, longitude, accuracy, context, place_id, woeid, tags, dateupload, datetaken, ownername, description, license, url_o, width_o, height_o, url_largest, height_largest, width_largest, largest_size, the_geom) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, ST_SetSRID(ST_MakePoint($8, $7), 4326))";
-
-  if (!park) {
-    park = { id: null, name: null };
-  }
-
-  if (photos) photos.forEach(function(photo) {
-    if (photo) {
-      //console.log(photo);
-      var params = [photo.id, photo.owner, photo.secret, photo.server, photo.farm, photo.title, photo.latitude, photo.longitude, photo.accuracy, photo.context, photo.place_id, photo.woeid, photo.tags, photo.dateupload, photo.datetaken, photo.ownername];
-      if (photo.description.length > 0)
-        params.push(photo.description._content);
-      else
-        params.push("");
-      // Here, figure out which is the largest photo size, and use that instead of url_l. Then save largest_size, too.
-      var url_largest = photo.url_s;
-      var width_largest = photo.width_s;
-      var height_largest = photo.height_s;
-      var largest_size = "s";
-      if (photo.url_m) {
-        url_largest = photo.url_m;
-        width_largest = photo.width_m;
-        height_largest = photo.height_m;
-        largest_size = "m";
-      }
-      if (photo.url_n) {
-        url_largest = photo.url_n;
-        width_largest = photo.width_n;
-        height_largest = photo.height_n;
-        largest_size = "n";
-      }
-      if (photo.url_z) {
-        url_largest = photo.url_z;
-        width_largest = photo.width_z;
-        height_largest = photo.height_z;
-        largest_size = "z";
-      }
-      if (photo.url_c) {
-        url_largest = photo.url_c;
-        width_largest = photo.width_c;
-        height_largest = photo.height_c;
-        largest_size = "c";
-      }
-      if (photo.url_l) {
-        url_largest = photo.url_l;
-        width_largest = photo.width_l;
-        height_largest = photo.height_l;
-        largest_size = "l";
-      }
-      params.push(photo.license, photo.url_o, photo.width_o, photo.height_o, url_largest, height_largest, width_largest, largest_size);
-      client.query(query, params, function(err, res) {
-        if (err) {
-          console.log("harvesterResults error", err.detail);
-          //client.end();
-          //startPostgresClient(function(err, client) { return client;});
-          //throw err;
-        }
-      });
-    }
-  });
-  // return nothing?
-};
-
 
 // Similar venues:
 // https://api.foursquare.com/v2/venues/VENUE_ID/similar
@@ -1307,24 +1110,6 @@ var getParksDataFromPostgres = function(client, socialMediaType, callback) {
   });
 };
 
-var getGridCellsFromPostgres = function(client, callback) {
-  //var query = "select distinct latmin, lngmin, latmax, lngmax from latlng_array order by latmin, lngmin, latmax, lngmax";
-  // Find only those needing update:
-  //var query = "select distinct a.latmin, a.lngmin, a.latmax, a.lngmax from latlng_array as a left join (select * from foursquare_metadata where date > (CURRENT_TIMESTAMP - INTERVAL '7 days')) as b on a.latmin = b.latmin and a.lngmin = b.lngmin and a.latmax = b.latmax and a.lngmax = b.lngmax where date is null order by a.latmin, a.lngmin, a.latmax, a.lngmax";
-  //var query = "select a.latmin, a.lngmin, a.latmax, a.lngmax, a.parks, b.date, b.count from (select latmin, lngmin, latmax, lngmax, array_agg(su_id) as parks from latlng_array group by latmin, lngmin, latmax, lngmax order by latmin, lngmin, latmax, lngmax) as a left join (select distinct on (latmin, lngmin, latmax, lngmax) * from foursquare_metadata where date > (CURRENT_TIMESTAMP - INTERVAL '7 days') order by latmin, lngmin, latmax, lngmax, date desc) as b on a.latmin = b.latmin and a.lngmin = b.lngmin and a.latmax = b.latmax and a.lngmax = b.lngmax order by a.latmin, a.lngmin, a.latmax, a.lngmax";
-  //var query = "select distinct latmin, lngmin, latmax, lngmax from latlng_array where latmin = 34.7 and lngmin = -120.5 order by latmin, lngmin, latmax, lngmax";
-  var query = "select distinct latmin, lngmin, latmax, lngmax from latlng_array order by latmin, lngmin desc, latmax, lngmax desc";
-
-  client.query(query, function(err, res) {
-    if (err) {
-      throw err;
-    }
-    console.log("got", res.rows.length, "grid cells from database that need harvesting");
-    var cells = res.rows;
-    callback(null, cells);
-  });
-};
-
 var getCirclesFromPostgres = function(client, callback) {
   var query = "select lat, lng, radius, array_agg(su_id) as parks from instagram_array group by lat, lng, radius order by lat desc, lng, radius";
   // A few around the bay area
@@ -1348,39 +1133,6 @@ var getInstagramPhotosForAllCircles = function(callback) {
       getInstagramPhotosForCircles(client, circles, function(err) {
         console.log("[*] done!");
         callback();
-      });
-    });
-  });
-};
-
-var getFlickrPhotosForAllGridCells = function(callback) {
-  return startPostgresClient(function(err, client) {
-    return getGridCellsFromPostgres(client, function(err, cells) {
-      async.eachLimit(cells, 1, function(cell, next) {
-        getFlickrPhotosForBbox(client, cell.latmin, cell.lngmin, cell.latmax, cell.lngmax, function(err, media, pages) {
-          var length = media ? media.length : 0;
-          console.log("[*] got", length, "photos for ", cell.latmin, cell.lngmin, cell.latmax, cell.lngmax, "after", pages, "page(s)");
-          next();
-        });
-      }, function() {
-        console.log("[*] done!");
-        callback();
-      });
-    });
-  });
-};
-
-var getFlickrPhotosForAllParks = function() {
-  return startPostgresClient(function(err, client) {
-    return getParksDataFromPostgres(client, "flickr", function(err, parks) {
-      async.eachLimit(parks, 1, function(park, next) {
-        getFlickrPhotosForPark(client, park, function(err, media, pages) {
-          console.log("[*] got", media.length, "photos for", park.id, park.name, "after", pages, "page(s)");
-          next();
-        });
-      }, function() {
-        console.log("[*] done!");
-        //client.end();
       });
     });
   });
@@ -1596,7 +1348,7 @@ var main = function() {
 
   if (argv.t == 'flickr') {
 
-    getFlickrPhotosForAllGridCells(process.exit);
+    flickr.getPhotosForAllGridCells(process.exit);
 
   } else if (argv.t == 'foursquare_venues') {
 
