e572fb80621bb1930164ec3caab7785a8de13aa0
Seth Fitzsimmons <seth@mojodna.net>
2014-10-04 12:28:29 -0700

    Fleshed out Instagram worker
    Now uses JSON for Instagram metadata

--- sql/instagram_photos.sql
+++ sql/instagram_photos.sql
@@ -1,39 +1,21 @@
 DROP TABLE IF EXISTS instagram_photos;
 CREATE TABLE instagram_photos (
   id serial PRIMARY KEY,
-  photoid varchar(40) UNIQUE,
-  attribution varchar,
-  latitude float,
-  longitude float,
-  placename varchar,
-  placeid bigint,
-  commentcount int,
-  filter varchar(20),
-  created_time bigint,
-  link varchar,
-  likescount int,
-  standard_resolution varchar,
-  width int,
-  height int,
-  caption varchar,
-  username varchar(40),
-  website varchar,
-  profile_picture varchar,
-  bio varchar,
-  userid bigint,
-  the_geom geometry(Point, 4326)
+  photo_id varchar(40) UNIQUE,
+  metadata json,
+  geom geometry(Point, 4326)
 );
 
-CREATE INDEX instagram_photos_the_geom_gist ON instagram_photos USING GIST(the_geom);
+CREATE INDEX instagram_photos_geom_gist ON instagram_photos USING GIST(geom);
 
--- TODO duplicates update_flickr_photos
+-- TODO duplicates update_flickr_photos except for the geometry transformation
 CREATE OR REPLACE FUNCTION update_instagram_photos() RETURNS TRIGGER AS $$
   BEGIN
     IF (TG_OP = 'INSERT') THEN
       -- check intersection on INSERT
       PERFORM superunit_id
-      FROM cpad_superunits
-      WHERE ST_Intersects(ST_Transform(NEW.the_geom, ST_SRID(geom)), geom);
+      FROM cpad_superunits cpad
+      WHERE ST_Intersects(ST_Transform(NEW.geom, ST_SRID(cpad.geom)), cpad.geom);
 
       IF NOT FOUND THEN
         RETURN NULL; -- don't insert
@@ -52,4 +34,4 @@ CREATE OR REPLACE RULE instagram_photos_ignore_duplicate_inserts AS
   ON INSERT TO instagram_photos
     WHERE (EXISTS (SELECT 1
       FROM instagram_photos
-      WHERE instagram_photos.photoid = NEW.photoid)) DO INSTEAD NOTHING;
+      WHERE instagram_photos.photo_id = NEW.photo_id)) DO INSTEAD NOTHING;
