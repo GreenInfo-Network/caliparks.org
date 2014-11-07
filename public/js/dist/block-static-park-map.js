dde0478591bdb3484f710a1056e62afc3d166a19
Eric Gelinas <evg@stamen.com>
2014-11-06 16:44:09 -0800

    tune map zoom a bit

--- public/js/dist/block-static-park-map.js
+++ public/js/dist/block-static-park-map.js
@@ -35,7 +35,9 @@ define([ "require", "exports", "module", "detect-os", "stamen-super-classy", "gm
                     mapTypeIds: [ "parksLayer" ]
                 }
             }), that.bigMap.mapTypes.set("parksLayer", that.parksLayer), that.bigMap.setMapTypeId("parksLayer"), 
-            that.bigMap.fitBounds(geoJSONBBoxToGoogleBounds(viewOptions.bbox)), google.maps.event.addDomListener(window, "resize", function() {
+            that.bigMap.fitBounds(geoJSONBBoxToGoogleBounds(viewOptions.bbox)), setTimeout(function() {
+                that.bigMap.setZoom(that.bigMap.getZoom() + 1);
+            }, 200), google.maps.event.addDomListener(window, "resize", function() {
                 google.maps.event.trigger(that.bigMap.getCenter(), "resize"), that.bigMap.setCenter(that.bigMap.getCenter());
             });
         }
