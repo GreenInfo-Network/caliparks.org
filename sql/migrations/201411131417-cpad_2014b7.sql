8724c05910975bcdb7d40ac0772f1be7de6299b9
Seth Fitzsimmons <seth@mojodna.net>
2014-11-13 15:25:53 -0800

    Update foursquare venues

--- sql/migrations/201411131417-cpad_2014b7.sql
+++ sql/migrations/201411131417-cpad_2014b7.sql
@@ -1,13 +1,16 @@
 -- Stillwater Cove Regional Park
 UPDATE flickr_photos SET superunit_id = 27985 WHERE superunit_id IN (12640, 1756);
+UPDATE foursquare_venues SET superunit_id = 27985 WHERE superunit_id IN (12640, 1756);
 UPDATE instagram_photos SET superunit_id = 27985 WHERE superunit_id IN (12640, 1756);
 
 -- Lynch Canyon
 UPDATE flickr_photos SET superunit_id = 12640 WHERE superunit_id = 12543;
+UPDATE foursquare_venues SET superunit_id = 12640 WHERE superunit_id = 12543;
 UPDATE instagram_photos SET superunit_id = 12640 WHERE superunit_id = 12543;
 
 -- Truckee River Wildlife Area
 UPDATE flickr_photos SET superunit_id = 12543 WHERE superunit_id IN (1327, 1328, 1332);
+UPDATE foursquare_venues SET superunit_id = 12543 WHERE superunit_id IN (1327, 1328, 1332);
 UPDATE instagram_photos SET superunit_id = 12543 WHERE superunit_id IN (1327, 1328, 1332);
 
 DROP MATERIALIZED VIEW cpad_superunits;
