293636f958210691d2b93d21e94eb7419f9a494e
Eric Gelinas <evg@stamen.com>
2014-11-21 10:00:17 -0800

    tweaks to viewport sizing

--- public/js/dist/view_parks.js
+++ public/js/dist/view_parks.js
@@ -1,16 +1,3 @@
-define([ "require", "exports", "module", "jquery", "block-activity-filter", "block-search-box", "stamen-super-classy" ], function(require, exports, module, jquery, BlockActivityFilter, BlockSearchBox, StamenSuperClassy) {
-    "use strict";
-    function TabletViewportManager() {
-        function resize() {
-            contentNode.style.height = window.innerHeight - heightOffset + "px", tabNode.style.height = window.innerHeight - heightOffset + "px";
-        }
-        var that = this;
-        StamenSuperClassy.apply(that, arguments);
-        var bodyNode = that.utils.get("body")[0], headerNode = that.utils.get("header", bodyNode)[0], footerNode = that.utils.get("footer", bodyNode)[0], contentNode = that.utils.get(".content", bodyNode)[0], tabNode = that.utils.get(".search-results", bodyNode)[0], heightOffset = (headerNode ? headerNode.offsetHeight : 0) + (footerNode ? footerNode.offsetHeight : 0);
-        return $(window).on("resize orientationChanged", function() {
-            resize();
-        }), resize(), that;
-    }
-    new BlockSearchBox(".block-search-box", {}, function() {}), new BlockActivityFilter(".block-activity-filter", {}, function() {}), 
-    new TabletViewportManager();
+define([ "require", "exports", "module", "jquery", "block-activity-filter", "block-search-box", "stamen-super-classy" ], function(require, exports, module, jquery, BlockActivityFilter, BlockSearchBox) {
+    new BlockSearchBox(".block-search-box", {}, function() {}), new BlockActivityFilter(".block-activity-filter", {}, function() {});
 });
--- public/js/view_parks.js
+++ public/js/view_parks.js
@@ -8,36 +8,7 @@ define(["require","exports","module","jquery","block-activity-filter","block-sea
   StamenSuperClassy
 ) {
 
-  "use strict";
-
-  function TabletViewportManager() {
-    var that = this;
-
-    StamenSuperClassy.apply(that, arguments);
-
-    var bodyNode     = that.utils.get('body')[0],
-        headerNode   = that.utils.get('header', bodyNode)[0],
-        footerNode   = that.utils.get('footer', bodyNode)[0],
-        contentNode  = that.utils.get('.content', bodyNode)[0],
-        tabNode  = that.utils.get('.search-results', bodyNode)[0],
-        heightOffset = (headerNode ? (headerNode.offsetHeight) : 0) + (footerNode ? footerNode.offsetHeight : 0);
-
-    function resize () {
-      contentNode.style.height = (window.innerHeight - heightOffset) + 'px';
-      tabNode.style.height = (window.innerHeight - heightOffset) + 'px';
-    }
-
-    $(window).on('resize orientationChanged', function() {
-      resize ();
-    });
-
-    resize();
-
-    return that;
-  }
-
   var blockSearchBox        = new BlockSearchBox(".block-search-box",{}, function(err, blockSearchBox) {});
   var blockActivityFilter   = new BlockActivityFilter(".block-activity-filter",{}, function(err, blockActivityFilter) {});
-  var tabletViewportManager = new TabletViewportManager();
 
 });
