bd7625732050bd8cc5fe1208fdf9a18a29eee515
Seth Fitzsimmons <seth@mojodna.net>
2014-12-02 10:34:38 -0800

    tabs -> spaces

--- controllers/parks.js
+++ controllers/parks.js
@@ -6,10 +6,10 @@ var cpad               = require('../lib/cpad.js'),
 
 module.exports = function(req, res, data, callback) {
 
-	var pg       = require('pg'),
-	    sorts    = {};
+  var pg       = require('pg'),
+      sorts    = {};
 
-	var contextDataDecorated, story;
+  var contextDataDecorated, story;
 
   function getActivityFilterState(withString) {
 
@@ -34,93 +34,93 @@ module.exports = function(req, res, data, callback) {
     return activities;
   }
 
-	//
-	// Is this a story context?
-	//
-	if (data.context === 'story') {
-		story = stories.getBySlug(req.params.query);
+  //
+  // Is this a story context?
+  //
+  if (data.context === 'story') {
+    story = stories.getBySlug(req.params.query);
 
-		if (story.parks) {
-			return cpad.getParksByIdList(story.parks, function(err, parks) {
+    if (story.parks) {
+      return cpad.getParksByIdList(story.parks, function(err, parks) {
 
-				if (err) {
-					return callback(err);
-				}
+        if (err) {
+          return callback(err);
+        }
 
-				go(err, parks);
-			});
-		}
-	}
+        go(err, parks);
+      });
+    }
+  }
 
-	//
-	// Add querystring but don't clobber anything called query from the caller
-	//
-	if (data.context === 'with') {
-		data.query = {
-			q    : req.query.q || '',
-			near : req.params.near  || req.query.near || null,
-			with : req.params.query || null
-		};
-	} else if (data.context === 'near') {
+  //
+  // Add querystring but don't clobber anything called query from the caller
+  //
+  if (data.context === 'with') {
+    data.query = {
+      q    : req.query.q || '',
+      near : req.params.near  || req.query.near || null,
+      with : req.params.query || null
+    };
+  } else if (data.context === 'near') {
 
-		//
-		// In this case, return nothing to the template so it
-		// can attempt to geolocate on the client
-		//
-		if (!req.params.query) {
-			callback(null,[]);
-		}
+    //
+    // In this case, return nothing to the template so it
+    // can attempt to geolocate on the client
+    //
+    if (!req.params.query) {
+      callback(null,[]);
+    }
 
-		data.query = {
-			q    : req.query.q || '',
-			near : req.params.query,
-			with : req.params.with  || req.query.with || null
-		};
-	} else if (data.context === 'story') {
-		data.query = {
-			q    : story.q    || '',
-			near : story.near || null,
-			with : story.with || null
-		};
-	} else if (req.params.query || req.query.q || req.query.near || req.query.with) {
-		data.query = {
-			q    : req.params.query || req.query.q || '',
-			near : req.query.near || null,
-			with : req.query.with || null
-		};
-	} else {
-		data.query = {
-			q    : req.params.query || req.query.q || '',
-			near : null,
-			with : null
-		};
-	}
+    data.query = {
+      q    : req.query.q || '',
+      near : req.params.query,
+      with : req.params.with  || req.query.with || null
+    };
+  } else if (data.context === 'story') {
+    data.query = {
+      q    : story.q    || '',
+      near : story.near || null,
+      with : story.with || null
+    };
+  } else if (req.params.query || req.query.q || req.query.near || req.query.with) {
+    data.query = {
+      q    : req.params.query || req.query.q || '',
+      near : req.query.near || null,
+      with : req.query.with || null
+    };
+  } else {
+    data.query = {
+      q    : req.params.query || req.query.q || '',
+      near : null,
+      with : null
+    };
+  }
 
-	cpad.getParks(data, function(err, parks) {
+  cpad.getParks(data, function(err, parks) {
 
-		go(err, parks);
+    go(err, parks);
 
-	});
+  });
 
-	function go(err, parks) {
-		if (err) {
-			return callback(err);
-		}
+  function go(err, parks) {
+    if (err) {
+      return callback(err);
+    }
 
-		data.options = {
-			startat : (req.query.startat|0) || 0,
-			perpage : (req.query.perpage|0) || 100,
-			not     : req.query.not || null
-		}
+    data.options = {
+      startat : (req.query.startat|0) || 0,
+      perpage : (req.query.perpage|0) || 100,
+      not     : req.query.not || null
+    }
 
-		return callback(null, {
-			parks          : parks,
-			total          : parks.length,
-			startat        : data.options.startat,
-			perpage        : data.options.perpage,
-			query          : data.query,
+    return callback(null, {
+      parks          : parks,
+      total          : parks.length,
+      startat        : data.options.startat,
+      perpage        : data.options.perpage,
+      query          : data.query,
       activities     : getActivityFilterState(data.query.with)
-		});
-	}
+    });
+  }
 
 }
