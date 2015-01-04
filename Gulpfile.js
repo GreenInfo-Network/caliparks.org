e940a907385859fe3bb9c844120c0e60542062c4
Eric Gelinas <eric@standardpixel.com>
2015-01-04 13:38:42 -0800

    move sass out of gulp until gulp-sass gets fixed

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,31 +1,15 @@
 var gulp           = require('gulp'),
-    sass           = require('gulp-sass'),
-    neat           = require('node-neat').includePaths,
     watch          = require('gulp-watch'),
     rename         = require('gulp-rename'),
     handlebars     = require("gulp-compile-handlebars"),
     jshint         = require('gulp-jshint'),
-    uglify         = require('gulp-uglify'),
-    //autopolyfiller = require('gulp-autopolyfiller'),
-    autoprefixer   = require('gulp-autoprefixer');
+    uglify         = require('gulp-uglify');
 
 var paths = {
     scss      : './sass/*.scss',
     templates : './views/*.handlebars',
     js        : './public/js/*.js'
 };
-
-gulp.task('styles', function () {
-  return gulp.src(paths.scss)
-      .pipe(sass({
-          includePaths: ['styles'].concat(neat)
-      }))
-      .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-      }))
-      .pipe(gulp.dest('./public/style'));
-});
 
 gulp.task('lint', function() {
   return gulp.src(paths.js)
@@ -58,24 +42,13 @@ gulp.task('uglify', function() {
     .pipe(gulp.dest('./public/js/dist'))
 });
 
-gulp.task('polyfill', function() {
-  gulp.src(paths.js)
-    .pipe(autopolyfiller('polyfills.js'))
-    .pipe(gulp.dest('./public/js/helpers'));
-});
-
 gulp.task('default',function(){
   gulp.start('lint');
-  //gulp.start('polyfill');
   gulp.start('uglify');
-  gulp.start('styles');
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch(paths.js, ['lint','uglify'/*, 'polyfill'*/]);
+    gulp.watch(paths.js, ['lint','uglify']);
     console.log('watching directory:' + paths.js);
-
-    gulp.watch(paths.scss, ['styles']);
-    console.log('watching directory:' + paths.scss);
 });
