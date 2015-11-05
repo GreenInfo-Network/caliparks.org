0630abfbefb08af92fc6b89348646d357f11cd1c
Seth Fitzsimmons <seth@mojodna.net>
2015-11-05 11:04:30 -0800

    Introduce redux-logger

--- app/public/store.js
+++ app/public/store.js
@@ -1,12 +1,17 @@
 import {applyMiddleware, createStore} from 'redux';
+import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
 import reducer from './reducer';
 
+const middleware = [thunkMiddleware];
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware
-)(createStore);
+if (typeof __REACT_ENGINE__ !== 'undefined') {
+  // redux-logger only works in a browser environment
+  middleware.push(createLogger());
+}
+
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
 
 export default function makeStore(initialState) {
   const store = createStoreWithMiddleware(reducer, initialState);
