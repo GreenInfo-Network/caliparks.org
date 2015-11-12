98c13864dc8c64e57f0eb9af79fa8552bc8342a9
Seth Fitzsimmons <seth@mojodna.net>
2015-11-12 12:42:47 -0800

    Flipped prod logic

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,14 +1,9 @@
-import path from 'path';
-
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import SvgStore from 'webpack-svgstore-plugin';
-
 let config;
 
 if (process.env.NODE_ENV === 'production') {
-  config = require('./webpack.config.dev.babel.js');
-} else {
   config = require('./webpack.config.prod.babel.js');
+} else {
+  config = require('./webpack.config.dev.babel.js');
 }
 
 export default config;
