0b82d55d135e5fe98a99203a2cd469fecf93f6d4
Sean Connelley <connelley@stamen.com>
2015-11-12 20:41:30 -0500

    Ugh actually adding in custom zoom controls

---
+++ app/public/components/gmapControls.jsx
@@ -0,0 +1,84 @@
+import {
+  default as React,
+  PropTypes,
+  Component
+} from 'react';
+
+import {
+  default as canUseDOM,
+} from 'can-use-dom';
+
+import {default as GoogleMapHolder} from 'react-google-maps/lib/creators/GoogleMapHolder';
+
+export default class GmapControls extends Component {
+
+  static propTypes = {
+    mapHolderRef: PropTypes.instanceOf(GoogleMapHolder)
+  }
+
+  static defaultProps = {
+    mapHolderRef: null,
+    tileUrl: null,
+    subdomains: ['a', 'b', 'c', 'd'],
+    tileSize: 256
+  };
+
+  state = {}
+
+  componentWillMount() {
+    if (!canUseDOM) {
+      return;
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.mapHolderRef) {
+      const map = this.props.mapHolderRef.getMap();
+      const centerControlDiv = document.createElement('div');
+      const centerControl = this.centerControl(centerControlDiv, map);
+      centerControlDiv.index = 1;
+      map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(centerControlDiv);
+    }
+  }
+
+  componentDidUpdate(prevProps) {}
+
+  componentWillUnmount() {}
+
+  centerControl(controlDiv, map) {
+    // Set CSS for the control border.
+    const controlUI = document.createElement('div');
+    controlUI.className = 'zoom-control-container';
+    controlDiv.appendChild(controlUI);
+
+    // Set CSS for the control interior.
+    const zoomIn = document.createElement('div');
+    zoomIn.className = 'zoom-in';
+    zoomIn.innerHTML = '+';
+    controlUI.appendChild(zoomIn);
+
+    // divider
+    const divider = document.createElement('div');
+    divider.className = 'divider';
+    controlUI.appendChild(divider);
+
+    const zoomOut = document.createElement('div');
+    zoomOut.className = 'zoom-out';
+    zoomOut.innerHTML = '–';
+    controlUI.appendChild(zoomOut);
+
+    google.maps.event.addDomListener(zoomIn, 'click', function() {
+      map.setZoom(map.getZoom() + 1);
+    });
+
+    google.maps.event.addDomListener(zoomOut, 'click', function() {
+      map.setZoom(map.getZoom() - 1);
+    });
+
+    return controlUI;
+  }
+
+  render() {
+    return (<noscript />);
+  }
+}
