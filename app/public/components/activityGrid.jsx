45fca4a115a6644eaaf07c668c890c8fb37db284
Sean Connelley <connelley@stamen.com>
2015-12-08 16:00:54 -0500

    Revised no activities error msg, #541

--- app/public/components/activityGrid.jsx
+++ app/public/components/activityGrid.jsx
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import PureComponent from 'react-pure-render/component';
+import {FormattedMessage} from 'react-intl';
 import { Link } from 'react-router';
 import {helpers} from '../../constants/park-activities';
 
@@ -17,7 +18,16 @@ export default class ActivityGrid extends PureComponent {
   componentDidUpdate() {}
 
   renderActivities() {
-    if (!this.props.activities) return (<h5 className='error'>No activities!</h5>);
+    if (!this.props.activities) {
+      return (
+        <p className='error'>
+        <FormattedMessage
+          id='park-page.no-activities'
+          defaultMessage='No activities data available.' />
+        </p>
+      );
+    }
+
     const {formatMessage} = this.props.intl;
 
     return this.props.activities.map((activity, index) => {
@@ -35,8 +45,9 @@ export default class ActivityGrid extends PureComponent {
   }
 
   render() {
+    const klass = (this.props.activities) ? 'activity-grid' : 'activity-grid no-data';
     return (
-      <div className='activity-grid'>
+      <div className={klass}>
         {this.renderActivities()}
       </div>
     );
