014aeb2ccc3402c2f36490d3ae34ad77deee992f
Greg Allensworth <gregor@greeninfo.org>
2017-07-13 11:32:34 -0700

    #645 GIN legend logo first draft demo

--- app/public/components/gmapControls.jsx
+++ app/public/components/gmapControls.jsx
@@ -34,15 +34,33 @@ export default class GmapControls extends Component {
   componentDidMount() {
     if (this.props.mapHolderRef) {
       const map = this.props.mapHolderRef.getMap();
+
       const centerControlDiv = document.createElement('div');
       centerControlDiv.index = 1;
       map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(this.centerControl(centerControlDiv, map));
+
+      const creditsControlDiv = document.createElement('div');
+      creditsControlDiv.index = 1;
+      map.controls[google.maps.ControlPosition.BOTTOM_RIGHT].push(this.creditsControl(creditsControlDiv));
     }
   }
 
   componentDidUpdate(prevProps) {}
 
   componentWillUnmount() {}
+
+  creditsControl(controlDiv) {
+    const controlUI = document.createElement('div');
+    controlUI.className = 'credits-control-container';
+    controlDiv.appendChild(controlUI);
+    controlUI.innerHTML = '<span>Parks data by <a target="_blank" href="http://www.greeninfo.org/">GreenInfo Network</a><br/>Other data &copy; <a target="_blank" href="https://www.openstreetmap.org/">OpenStreetMap</a></span>';
+
+    google.maps.event.addDomListener(controlUI, 'click', function() {
+      controlUI.classList.toggle('credits-control-expanded');
+    });
+
+    return controlUI;
+  }
 
   centerControl(controlDiv, map) {
     // Set CSS for the control border.
